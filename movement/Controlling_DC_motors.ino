dde4f86c2a3ab2be66f84db6294b1372531399c9
ellak <ellak@localhost.localdomain>
2015-05-19 21:01:57 +0300

    updated with Serial Helper Library

--- movement/Controlling_DC_motors.ino
+++ movement/Controlling_DC_motors.ino
@@ -1,80 +1,103 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-
+#include <SerialHelper.h>
+ 
+SerialHelper serialHelper(128, 0);
 // Setting up Motors on Adafruit Motorshield
 // Left Motor is Motor 4 and Right Motor is Motor 3
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *myMotor2 = AFMS.getMotor(4);
 
-#define MOTORSPEED 255 //255 is maximum speed
+int leftSpeed = 0;
+int rightSpeed = 0;
 
 void setup() {
   Serial.begin(9600); // set up Serial library at 9600 bps
-  
   AFMS.begin();
-  myMotor->setSpeed(150);
+  myMotor->setSpeed(leftSpeed);
   myMotor->run(FORWARD);
   // turn on motor
   myMotor->run(RELEASE);
-  myMotor2->setSpeed(150);
+  myMotor2->setSpeed(rightSpeed);
   myMotor2->run(FORWARD);
   // turn on motor
   myMotor2->run(RELEASE);
 }
 void loop() {
   uint8_t i;
+
+
+
   if(Serial.available() > 0){
-    char test = Serial.read();
+    
+    //leftSpeed = serialHelper.read();
+    //rightSpeed = Serial.read();
+    
+    serialHelper.read();
+    char *test = serialHelper.getMessage();
+    int len = serialHelper.getMessageLength();
     //Serial.println();
     //Serial.println(test);
-    if(test == 'w'){
-      //Serial.print("Forward ");
+    if(strcmp(test, "w") == 0){
+       serialHelper.write(test, len);
       moveForward(myMotor, myMotor2);
-    } 
-    else if (test == 's'){
-      //Serial.print("Backward ");
+    }
+    else if (strcmp(test, "s") == 0){
+       serialHelper.write(test, len);
       moveBackward(myMotor, myMotor2);
-    } 
-    else if (test == 'a'){
-      //Serial.print("Left");
+    }
+    else if (strcmp(test, "a") == 0){
+       serialHelper.write(test, len);
       moveLeft(myMotor, myMotor2);
-    } 
-    else if (test == 'd'){
-      //Serial.print("Right");
+    }
+    else if (strcmp(test, "d") == 0){
+       serialHelper.write(test, len);
       moveRight(myMotor, myMotor2);
     }
-    else if(test == 'q'){
+    else if(strcmp(test, "q") == 0){
+       serialHelper.write(test, len);
       myMotor->run(RELEASE);
       myMotor2->run(RELEASE);
     }
   }
 }
+
+
+void invert(char *buff, int len) {
+  int i, tmp;
+
+  for( i = 0 ; i < len / 2 ; i++ ) {
+      tmp = buff[len - i - 1];
+      buff[len - i - 1] = buff[i];
+      buff[i] = tmp;
+    }
+}
 void moveForward(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(FORWARD);
   motor2->run(FORWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(leftSpeed);
+  motor2->setSpeed(rightSpeed);
 }
 void moveBackward(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(BACKWARD);
   motor2->run(BACKWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(leftSpeed);
+  motor2->setSpeed(rightSpeed);
 }
 void moveRight(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(BACKWARD);
   motor2->run(FORWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(leftSpeed);
+  motor2->setSpeed(rightSpeed);
 }
 void moveLeft(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(FORWARD);
   motor2->run(BACKWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(leftSpeed);
+  motor2->setSpeed(rightSpeed);
 }

53ff01b63d8e6cd6fbfc25e3361d50c9fe4ab3d4
Dimitris Andritsakis <dandrits@teiath.gr>
2015-06-04 08:02:49 +0300

    added speed functionality

--- movement/Controlling_DC_motors.ino
+++ movement/Controlling_DC_motors.ino
@@ -8,35 +8,63 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *myMotor2 = AFMS.getMotor(4);
 
-#define MOTORSPEED 255 //255 is maximum speed
+int motorspeedRight = 255;
+int motorspeedLeft = 252;
+
+//#define MOTORSPEED 255 //255 is maximum speed
 void setup() {
   Serial.begin(9600); // set up Serial library at 9600 bps
+AFMS.begin();
+  myMotor->setSpeed(0);
+  myMotor->run(FORWARD);
+  // turn on motor
+  myMotor->run(RELEASE);
+  myMotor2->setSpeed(0);
+  myMotor2->run(FORWARD);
+  // turn on motor
+  myMotor2->run(RELEASE);
 }
+
+
 void loop() {
   uint8_t i;
   if(Serial.available() > 0){
-    char test = Serial.read();
+      
+    //char test = Serial.read();
+    //Serial.println(test);
 
-    //Serial.println();
-    Serial.println(test);
-    if(test == 'w'){
+    String directionsData = Serial.readString();
+    Serial.println(directionsData);
+    
+    //String directions = directionsData.substring(0,1);
+    //Serial.println(directionsData.substring(1,4).toInt());
+    //Serial.println(directionsData.substring(5,8));
+
+    if(directionsData.startsWith("w")){
       Serial.println("Forward ");
+      
+      if(directionsData.length() > 1){
+        Serial.println("motorspeed");
+      motorspeedRight = directionsData.substring(1,4).toInt();
+      motorspeedLeft = directionsData.substring(5,8).toInt() - 3;
+      }
+      
       moveForward(myMotor, myMotor2);
     } 
-    else if (test == 's'){
+    else if (directionsData.startsWith("s")){
       Serial.println("Backward ");
       moveBackward(myMotor, myMotor2);
     } 
-    else if (test == 'a'){
+    else if (directionsData.startsWith("a")){
       Serial.println("Left");
       moveLeft(myMotor, myMotor2);
     } 
-    else if (test == 'd'){
+    else if (directionsData.startsWith("d")){
       Serial.println("Right");
       moveRight(myMotor, myMotor2);
     }
 
-    else if(test == 'q'){
+    else if(directionsData.startsWith("q")){
       Serial.println("Stop");
       myMotor->run(RELEASE);
       myMotor2->run(RELEASE);
@@ -48,27 +76,27 @@ void moveForward(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(FORWARD);
   motor2->run(FORWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(motorspeedLeft);
+  motor2->setSpeed(motorspeedRight);
 }
 void moveBackward(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(BACKWARD);
   motor2->run(BACKWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(motorspeedLeft);
+  motor2->setSpeed(motorspeedRight);
 }
 void moveRight(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(BACKWARD);
   motor2->run(FORWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(motorspeedLeft);
+  motor2->setSpeed(motorspeedRight);
 }
 void moveLeft(Adafruit_DCMotor *motor, Adafruit_DCMotor *motor2){
   uint8_t i;
   motor->run(FORWARD);
   motor2->run(BACKWARD);
-  motor->setSpeed(MOTORSPEED);
-  motor2->setSpeed(MOTORSPEED);
+  motor->setSpeed(motorspeedLeft);
+  motor2->setSpeed(motorspeedRight);
 }
